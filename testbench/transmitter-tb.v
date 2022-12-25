fe5adb6fd9afcf93e2ea818f4f8c77483e90b79f
Philopateer Moheb <104172766+PhilopateerMoheb1@users.noreply.github.com>
2022-12-25 23:37:36 +0200

    Update transmitter-tb.v

--- testbench/transmitter-tb.v
+++ testbench/transmitter-tb.v
@@ -1,35 +1,54 @@
+ 
 `include "components/baud-rate-generator.v"
+`include "components/fifo.v"
 `include "components/transmitter.v"
 
 `timescale 1ns/1ns
 module transmitter_tb;
 
-reg [7:0] din=8'b01010101;
-wire tx_done_tick;
-reg clk = 0,
-    s_tick = 0,
-    tx_start = 0;
-wire dout=1;
+reg [7:0] dataInTofifo;
+reg clk = 0,reset;
+reg writeEn=0;
 
 baud_gen br1 (
     .clk(clk),
-    .divsr(11'd650)
+    .divsr(11'd650),
+    .reset(reset)
 );
 
 Transmitter#(8,16)t1 (
-    .tx_start(tx_start),
+    .tx_start(~f.EMPTY),
     .s_tick(br1.tick),
-    .din(din),
-    .tx_done_tick(tx_done_tick),
-    .dout(dout)
+    .tx_dataIn(f.dataOut)
 );
-initial
-begin 
-$dumpfile("trasmitter.vcd");
-$dumpvars(0, transmitter_tb);
-  #10
-  tx_start=1;
+
+fifo f(
+  .clk(clk),
+  .dataIn(dataInTofifo),
+  .reset(reset),
+  .readEn(t1.tx_done_tick),
+  .writeEn(writeEn)
+);
+
+initial begin
+    $dumpfile("transmitter");
+    $dumpvars(0, transmitter_tb);
+
+    reset=1;
+    #15
+    reset=0;
+
+    dataInTofifo=8'b01010101;
+    writeEn=1;//put data in fifo
+    #15
+    writeEn = 0;
+    #15
+    writeEn = 1;
+    dataInTofifo=8'b01010111;
+    #15
+    writeEn=0;
+
 end
 
 always #5 clk = ~clk;
-endmodule  
+endmodule
