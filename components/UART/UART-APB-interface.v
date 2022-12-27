ca129304fdff99711a0095f7ebc44695ea556900
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2022-12-27 22:53:10 +0200

    feat: add UART_APB_interface component and tb

---
+++ components/UART/UART-APB-interface.v
@@ -0,0 +1,66 @@
+`timescale 1ns/1ns
+module UART_APB_interface(
+    // APB side
+    input PCLK,          
+    input PRESETn,       
+    input [31:0] PADDR,    
+    input [31:0] PWDATA, 
+    input PSELx,
+    input PWRITE,
+    input PENABLE,
+    output [31:0] PRDATA, 
+    output reg PREADY,
+    // UART side
+    input [7:0] rx_fifo_dataOut, 
+    input rx_fifo_Full,
+    input rx_fifo_Empty,
+    input tx_fifo_Full,
+    output reg tx_fifo_writeEn, 
+    output reg rx_fifo_readEn,  
+    output [7:0] tx_fifo_dataIn,  
+    output reg reset
+);
+localparam cycleDuration = 10;
+
+assign tx_fifo_dataIn = PWDATA[7:0];
+assign rx_fifo_dataOut = PRDATA[7:0];
+
+always @(posedge PCLK, posedge PRESETn) begin
+    if(PRESETn) begin
+        reset = 1;
+        #cycleDuration
+        reset = 0;
+    end
+    else if(PSELx == 1 && PENABLE == 1) begin
+        case(PWRITE) 
+            1: begin // write operation to the tx FIFO
+                /* 
+                    if the transmitter buffer is full uart cannot accept more data
+                    the master need to wait for a space in tx buffer,
+                */ 
+                PREADY = ~tx_fifo_Full;
+                tx_fifo_writeEn = 1;
+                #cycleDuration
+                tx_fifo_writeEn = 0;
+            end
+            0: begin // read from rx FIFO
+            /*
+                if the receiver buffer is empty, there is no data to read from 
+                the buffer.
+                the master need to wait for the next data to be received 
+                then it can read it.
+            */
+                PREADY = ~rx_fifo_Empty;
+                rx_fifo_readEn = 1;
+                #cycleDuration
+                rx_fifo_readEn = 0;
+            end
+        endcase
+    end
+    else if (PENABLE == 0) begin
+        PREADY = 0;
+    end
+end
+
+
+endmodule
---
+++ testbench/UART/UART_APB_interface_tb.v
@@ -0,0 +1,92 @@
+`include "components/UART/UART-APB-interface.v"
+`timescale 1ns/1ns
+
+module UART_APB_interface_tb;
+    reg PCLK = 0;          
+    reg PRESETn;       
+    reg PSELx;
+    reg PENABLE;
+    reg PWRITE;
+    reg  [31:0] PWDATA; 
+    reg rx_fifo_Full;
+    reg rx_fifo_Empty;
+    reg tx_fifo_Full;
+    reg [7:0] rx_fifo_dataOut;
+    wire [31:0] PADDR;   
+
+
+UART_APB_interface ua (
+    .PCLK(PCLK),
+    .PRESETn(PRESETn),
+    .PADDR(PADDR),
+    .PWDATA(PWDATA),
+    .PSELx(PSELx),
+    .PWRITE(PWRITE),
+    .PENABLE(PENABLE),
+    .rx_fifo_dataOut(rx_fifo_dataOut),
+    .rx_fifo_Full(rx_fifo_Full),
+    .rx_fifo_Empty(rx_fifo_Empty),
+    .tx_fifo_Full(tx_fifo_Full)
+);
+
+initial begin
+    $dumpfile("interface.vcd");
+    $dumpvars(0, ua);
+
+    rx_fifo_Full <= 0;
+
+
+    // reset
+    PRESETn <= 1;
+    #10 
+    PRESETn <= 0;
+
+    #20
+
+    // write operation tx not full
+    tx_fifo_Full <= 0;
+    PENABLE <= 1;
+    PSELx <= 1;
+    PWRITE <= 1;
+    PWDATA <= 8'b10101010;
+    #10
+    PENABLE <= 0;
+
+    #20
+
+    // write operation tx full 
+    tx_fifo_Full <= 1;
+    PENABLE <= 1;
+    PSELx <= 1;
+    PWRITE <= 1;
+    PWDATA <= 8'b00110011;
+    #10
+    PENABLE <= 0;
+
+
+    #20
+
+    // read operation rx fifo not empty
+    rx_fifo_Empty <= 0;
+    PENABLE <= 1;
+    PSELx <= 1;
+    PWRITE <= 0;
+    rx_fifo_dataOut <= 8'b11110000;
+    #10
+    PENABLE <= 0;
+
+    #20
+
+    // read operation rx fifo empty
+    rx_fifo_Empty = 1;
+    PENABLE <= 1;
+    PSELx <= 1;
+    PWRITE <= 0;
+    rx_fifo_dataOut <= 8'b11001100;
+    #10
+    PENABLE <= 0;
+    
+end
+
+always #5 PCLK <= ~PCLK;
+endmodule
